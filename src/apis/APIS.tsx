41d619f02f209ed1a7e83885e1db6244425c29a6
aponchikaj <lazzexa@gmail.com>
2025-12-27 16:34:30 +0400

    asjkdoifm

--- src/apis/APIS.tsx
+++ src/apis/APIS.tsx
@@ -1,6 +1,6 @@
 // Use Vite env `VITE_API_URL` when available (set in production), otherwise fallback to known backend.
 // const envApi = typeof import.meta !== 'undefined' ? (import.meta.env.VITE_API_URL as string | undefined) : undefined;
-export const MAIN_API_URL: string ='http://localhost:3001/api/';
+export const MAIN_API_URL: string ='https://alertup-backend.onrender.com/api/';
 export const APIS = {
     contact:`${MAIN_API_URL}contact`,
     me:`${MAIN_API_URL}me`,
