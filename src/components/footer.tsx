f770e8656f129d9aed3476953499d3643d2693fc
aponchikaj <lazzexa@gmail.com>
2026-01-19 10:44:19 +0400

    updated footer design

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,5 @@
-import TiktokIcon from '../assets/images/tt.png'
-import InstagramIcon from '../assets/images/ig.png';
+// import TiktokIcon from '../assets/images/tt.png'
+// import InstagramIcon from '../assets/images/ig.png';
 import { Link } from 'react-router-dom';
 import Sponsors from './sponsors';
 
@@ -24,17 +24,17 @@ const Footer = ()=>{
                     <Link to={'/'}>Home</Link>
                     <Link to={'/scan'}>Scan</Link>
                     <Link to={'/contact'}>Contact</Link>
-                    <Link to={'/premium'}>Premium</Link>
+                    {/* <Link to={'/premium'}>Premium</Link> */}
                 </ul>
             </section>
 
-            <section className="flex items-center justify-center gap-1 my-[10px]">
-                <a href=""><button><img src={TiktokIcon} alt="tiktok" className='w-[30px] cursor-pointer' /></button></a>
-                <a href=""><button><img src={InstagramIcon} alt="instagram" className='w-[30px] cursor-pointer' /></button></a>
-            </section>
+            {/* <section className="flex items-center justify-center gap-1 my-[10px]"> */}
+                {/* <a href=""><button><img src={TiktokIcon} alt="tiktok" className='w-[30px] cursor-pointer' /></button></a> */}
+                {/* <a href=""><button><img src={InstagramIcon} alt="instagram" className='w-[30px] cursor-pointer' /></button></a> */}
+            {/* </section> */}
 
             <section className="text-center items-center flex flex-col justify-center text-white text-sm">
-                <h1>By <a href="https://lazare-mirziashvili.vercel.app/home" className="underline hover:text-[#FF7B22] ease-in-out duration-100">Lazare</a></h1>
+                {/* <h1>By <a href="https://lazare-mirziashvili.vercel.app/home" className="underline hover:text-[#FF7B22] ease-in-out duration-100">Lazare</a></h1> */}
                 <p>Alertup &copy; {new Date().getFullYear()}</p>
             </section>
 
